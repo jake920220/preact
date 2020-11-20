67942d6622a0e8fd5752762fea28c6bf03afb3e6
jake920220 <jake220@naver.com>
2020-11-20 17:58:21 +0900

    FEAT: NavLink와 activeStyle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Fragment} from "react";
-import { Route } from "react-router-dom";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Index from "./pages/Index";
 import About from "./pages/About";
@@ -8,12 +8,17 @@ import SignUp from "./pages/SignUp";
 
 const App = () => {
     return (
-        <Fragment>
+        <Switch>
             <Route exact path="/" component={Index} />
             <Route path="/about" component={About} />
             <Route path="/login" component={Login} />
             <Route path="/sign-up" component={SignUp} />
-        </Fragment>
+            <Route 
+            render={({location}) => (
+                <div></div>
+            )}
+            />
+        </Switch>
     )
 }
 
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Index = () => {
+    const activeStyle = {
+        color: "red"
+    }
     return (
         <div>
             <h1>메인 페이지입니다.</h1>
             <ul>
-                <li><Link to={"/about"}>소개 페이지</Link></li>
-                <li><Link to={"/login"}>로그인 페이지</Link></li>
-                <li><Link to={"/sign-up"}>가입 페이지</Link></li>
+                <li><NavLink activeStyle={activeStyle} exact to={"/"}>메인 페이지</NavLink></li>
+                <li><NavLink activeStyle={activeStyle} to={"/about"}>소개 페이지</NavLink></li>
+                <li><NavLink activeStyle={activeStyle} to={"/login"}>로그인 페이지</NavLink></li>
+                <li><NavLink activeStyle={activeStyle} to={"/sign-up"}>가입 페이지</NavLink></li>
             </ul>
         </div>
     )
